8273d1f02179656590bf86905d045488bbbe26da
Michelle Ranly <meranly8@gmail.com>
2021-02-03 01:24:53 -0600

    commit tree 9dfab7b5237b77317d2baaae48c70f808e7c066c

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:index, :show] do
+    resources :posts, only: [:index, :show, :new, :edit]
+  end
+  resources :posts
 end
